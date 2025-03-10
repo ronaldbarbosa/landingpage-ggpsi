0ff7efbed48e145a3b1f1b9018a916afabdced3d
Ronald Barbosa <ronaldverissimo@hotmail.com>
2025-03-10 11:57:03 -0300

    feat: add nav component and fished first version for header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from "./nav/nav.component";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NavComponent, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
