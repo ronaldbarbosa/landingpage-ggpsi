363a2e7735edb8555a0950ca8b2349f394f9cd57
Ronald Barbosa <ronaldverissimo@hotmail.com>
2025-03-22 19:54:50 -0300

    feat: init implementation for about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component } from '@angular/core';
 import { HeaderComponent } from "../../layout/header/header.component";
 import { FooterComponent } from "../../layout/footer/footer.component";
+import { NgOptimizedImage } from '@angular/common'
+import { animate, scroll } from 'motion';
 
 @Component({
   selector: 'app-about',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, NgOptimizedImage],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  constructor() {
+    afterNextRender(() => this.initAnimations());
+  }
 
+  private initAnimations() {
+    animate('.fade-up', { opacity: [0, 1], y: [-20, 0] }, { duration: 1 });
+    animate('.fade-up-slowly', { opacity: [0, 1], y: [-50, 0] }, { duration: 1.5 });
+  }
 }
