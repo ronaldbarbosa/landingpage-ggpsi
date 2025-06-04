57e1e850cd600c2a20b2a9cd63952bd8a378cb30
Ronald Barbosa <ronaldverissimo@hotmail.com>
2025-06-04 20:18:53 -0300

    feat: configured i18n; added home, about and not found routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Gabriela Goia' },
+  { path: 'sobre', component: AboutComponent, title: 'Sobre Mim' },
+  { path: '**', component: NotFoundComponent, title: 'Página Não Encontrada' },
+];
