dd4f1a67b052c0bacac8fbcb305e6a4c9aabcb16
Ronald Barbosa <ronaldverissimo@hotmail.com>
2025-06-04 22:40:07 -0300

    feat: adjusted app component layout; create hamburger menu for mobile screen

--- src/app/components/header/nav/nav.component.ts
+++ src/app/components/header/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
+  showMenu = false;
 }
