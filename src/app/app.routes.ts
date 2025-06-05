2f66a93b2b97359e58be9bb6958624bd7cff6ffe
Ronald Barbosa <ronaldverissimo@hotmail.com>
2025-06-05 19:43:20 -0300

    feat: created contact, experiences and services pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
+import { ExperiencesComponent } from './pages/experiences/experiences.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Gabriela Goia' },
   { path: 'sobre', component: AboutComponent, title: 'Sobre Mim' },
+  { path: 'servicos', component: ServicesComponent, title: 'Serviços' },
+  { path: 'experiencia', component: ExperiencesComponent, title: 'Formação & Experiências' },
+  { path: 'contato', component: ContactComponent, title: 'Contato' },
   { path: '**', component: NotFoundComponent, title: 'Página Não Encontrada' },
 ];
--- src/app/components/header/nav/nav.component.ts
+++ src/app/components/header/nav/nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
