8890c30d72bc4f3b7163b71809ecb5d42adf6a9a
Ronald Barbosa <ronaldverissimo@hotmail.com>
2025-06-09 21:53:49 -0300

    feat: installed and configured bootstrap icons; defined main styles; implemented styles for header and footer components

--- src/app/components/footer/info/info.component.ts
+++ src/app/components/footer/info/info.component.ts
@@ -1,11 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-info',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './info.component.html',
   styleUrl: './info.component.css'
 })
 export class InfoComponent {
-
+  currentDate = new Date();
 }
--- src/app/components/header/nav/nav.component.ts
+++ src/app/components/header/nav/nav.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   showMenu = false;
+  links = [
+    { label: 'Inicio', path: '/' },
+    { label: 'Sobre', path: '/sobre' },
+    { label: 'Serviços', path: '/servicos' },
+    { label: 'Formação & Experiências', path: '/experiencia' },
+    { label: 'Contato', path: '/contato' }
+  ];
 }
